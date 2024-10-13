b79e5f25a69540095c4250c484b73200a5d6b18e
Daniel Griffiths <1dangriffiths@gmail.com>
2024-10-13 10:59:56 +0100

    add post item for list of blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
-import Link from "next/link";
+import PostItem from "@/components/PostItem";
+
+type Post = {
+  title: string;
+  href: string;
+  postDate: Date;
+};
+
+const posts: Post[] = [
+  {
+    title: `Finally, I'm writing a blog.`,
+    href: "/finally",
+    postDate: new Date("2024-10-13"),
+  },
+  {
+    title: `My thoughts on NextJS`,
+    href: "/next",
+    postDate: new Date("2022-10-13"),
+  },
+];
 
 export default function Home() {
   return (
     <div className={`flex flex-col gap-6`}>
       <h1>Posts</h1>
-      <ul className="list-disc pl-12">
-        <li>
-          <Link href="/finally">Finally, I'm writing a blog.</Link>
-        </li>
-        <li>
-          <Link href={"/next"}>My experience with NextJS</Link>
-        </li>
+      <ul className="list-none space-y-3">
+        {posts.map((post) => (
+          <PostItem
+            key={post.title}
+            postDate={post.postDate}
+            href={post.href}
+            title={post.title}
+          />
+        ))}
       </ul>
     </div>
   );
---
+++ components/PostItem.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function PostItem({
+  title,
+  href,
+  postDate,
+}: {
+  title: string;
+  href: string;
+  postDate: Date;
+}): JSX.Element {
+  return (
+    <div className="flex gap-6">
+      <p className="text-4xl">-</p>
+      <div className="flex flex-col">
+        <Link href={href}>{title}</Link>
+        <p className="text-gray-600 text-md">{postDate.toLocaleDateString()}</p>
+      </div>
+    </div>
+  );
+}
