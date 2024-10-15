c137d26c034daf176a57c2688b88528c37a8def3
Daniel Griffiths <1dangriffiths@gmail.com>
2024-10-15 15:33:21 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@ const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
   output: "export",
-  basePath: "/dan-blog",
+  images: { unoptimized: true },
 };
 
 module.exports = withMDX(nextConfig);
