038f98ae20fa352b29c92144359e7a343ef89812
Daniel Griffiths <1dangriffiths@gmail.com>
2024-10-15 15:14:15 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-const withMDX = require('@next/mdx')()
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Configure `pageExtensions` to include MDX files
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    // Optionally, add any other Next.js config below
-}
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+  output: "export",
+};
 
 module.exports = withMDX(nextConfig);
