a9ce4da88d1d9bcb4c57672ea2665a77b2e80c1b
Magpys <1dangriffiths@gmail.com>
2025-05-04 22:05:50 +0100

    reverse post order

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,17 @@ export default async function Home() {
     <div className={`flex flex-col gap-6`}>
       <h1>Posts</h1>
       <ul className="list-none space-y-3">
-        {posts.map((post) => (
-          <PostItem
-            key={post.title}
-            postDate={post.postDate}
-            href={`/posts/${post.slug}`}
-            title={post.title}
-            tags={post.tags}
-          />
-        ))}
+        {posts
+          .sort((a, b) => b.postDate.getTime() - a.postDate.getTime())
+          .map((post) => (
+            <PostItem
+              key={post.title}
+              postDate={post.postDate}
+              href={`/posts/${post.slug}`}
+              title={post.title}
+              tags={post.tags}
+            />
+          ))}
       </ul>
     </div>
   );
